85ab638d5df1ab7e3a4769101c5fc919d5cec223
didogrigorov <dido.grigorov@gmail.com>
2023-05-19 10:21:20 +0300

    hackerrank problems solved

--- finding_percentage.py
+++ finding_percentage.py
@@ -1,13 +1,19 @@
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
+
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
+
     query_name = input()
+
     grade_sum = 0
+
     for i in student_marks[query_name]:
         grade_sum += i
+
     average = grade_sum / len(student_marks[query_name])
+
     print(f"{average:.2f}")
